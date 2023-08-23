87640ff38acb16692c4a2bd2ae8bec45e96b578e
ankita.patel <ankita.patel@xmplify.tech>
2023-08-23 23:28:06 +0530

    Spring Boot Rest JPA Project :OneToOne mapping

---
+++ 04-spring-boot-rest-jpa/bootrestbook/src/main/java/com/api/book/bootrestbook/entity/Author.java
@@ -0,0 +1,65 @@
+package com.api.book.bootrestbook.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author_info")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int authorId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "language")
+    private String language;
+
+
+    public Author() {
+    }
+
+    public Author(int authorId, String firstName, String lastName, String language) {
+        this.authorId = authorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.language = language;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
